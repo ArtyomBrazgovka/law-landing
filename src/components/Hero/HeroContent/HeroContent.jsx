e2cef34944eb0ca13924aae71733a3c4dbdde9e3
Artyom Brazgovka <abrazgovka8@gmail.com>
2025-06-13 12:44:42 +0300

    feat: title added

--- src/components/Hero/HeroContent/HeroContent.jsx
+++ src/components/Hero/HeroContent/HeroContent.jsx
@@ -1,20 +1,19 @@
-import "./HeroContent.css"
+import "./HeroContent.css";
 
 export function HeroContent() {
-    return(
-        <div className="container-hero-content">
-            <div className="title">
-
-            </div>
-            <div className="request">
-
-            </div>
-            <div className="statue">
-
-            </div>
-            <div className="numbers">
-
-            </div>
+  return (
+    <div className="container-hero-content">
+      <div className="title">
+        <div className="upper-title">
+          <h1>Решим любой правовой вопрос</h1>
         </div>
-    )
+        <div className="lower-title">
+          <p>Помощь в решении любого правового вопроса в вашу пользу</p>
+        </div>
+      </div>
+      <div className="request"></div>
+      <div className="statue"></div>
+      <div className="numbers"></div>
+    </div>
+  );
 }
