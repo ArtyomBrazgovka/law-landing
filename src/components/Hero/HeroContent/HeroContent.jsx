dac1e188c265cafec793e8c8f0aaeea53d50907e
Artyom Brazgovka <abrazgovka8@gmail.com>
2025-06-16 10:40:28 +0300

    feat: statue added

--- src/components/Hero/HeroContent/HeroContent.jsx
+++ src/components/Hero/HeroContent/HeroContent.jsx
@@ -1,22 +1,25 @@
 import IMask from "imask";
 import "./HeroContent.css";
-import { useEffect, useRef } from 'react';
-import "./BlockNumber/BlockNumber.jsx"
+import { useEffect, useRef } from "react";
+import "./BlockNumber/BlockNumber.jsx";
 import { BlockNumber } from "./BlockNumber/BlockNumber.jsx";
 
+import statue from "/images/statue.png";
+
+import lists from "/images/lists.png";
 
 export function HeroContent() {
-    const phoneInputRef = useRef(null);
+  const phoneInputRef = useRef(null);
 
-    useEffect(() => {
+  useEffect(() => {
     if (!phoneInputRef.current) return;
-    
+
     const mask = IMask(phoneInputRef.current, {
-      mask: '+{7} (000) 000-00-00',
+      mask: "+{7} (000) 000-00-00",
       lazy: false,
       definitions: {
-        '0': /[0-9]/ 
-      }
+        0: /[0-9]/,
+      },
     });
 
     return () => {
@@ -36,20 +39,28 @@ export function HeroContent() {
       <div className="request">
         <p className="request-title">Нужна консультация?</p>
         <form className="request-form">
-          <input type="tel" className="request-phone" ref={phoneInputRef} required></input>
-          
-
+          <input
+            type="tel"
+            className="request-phone"
+            ref={phoneInputRef}
+            inputMode="text"
+            required
+          ></input>
           <button className="request-send">Заказать звонок</button>
         </form>
       </div>
-      
+
       <div className="numbers">
-        <BlockNumber number="89%" title="Выигранных дел"/>
-        <BlockNumber number="34" title="Сотрудника в штате"/>
-        <BlockNumber number="18" title="Лет на рынке"/>
+        <BlockNumber number="89%" title="Выигранных дел" />
+        <BlockNumber number="34" title="Сотрудника в штате" />
+        <BlockNumber number="18" title="Лет на рынке" />
       </div>
 
-      <div className="statue"></div>
+      <div className="statue">
+        <img class = "lists" src = {lists} alt = "Листы"></img>
+        <img class = "statue-img" src = {statue} alt = "Статуя"></img>
+        
+      </div>
     </div>
   );
 }
