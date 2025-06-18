b5e7b336d8bccfe86e95c068fe9d012c70a69c17
Artyom Brazgovka <abrazgovka8@gmail.com>
2025-06-18 09:27:29 +0300

    feat: background added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css"
 
 import { Hero } from "./components/Hero/Hero.jsx";
 import { Director } from "./components/Director/Director.jsx";
+import { Advantages } from "./components/Advantages/Advantages.jsx";
 
 function App() {
   return (
     <div className="app-container">
       <Hero/>
       <Director/>
+      <Advantages/>
     </div>
     
   );
---
+++ src/components/Advantages/Advantages.jsx
@@ -0,0 +1,15 @@
+import "./Advantages.css"
+
+export function Advantages(){
+    return(
+        <div className="advantages-section">
+            <div className="advantages-container">
+                <div className="advantages-title">
+                    <p className="advantages-headline"></p>
+                    <h2 className="advantages-subtitle"></h2>
+                </div>
+                <div className="advantages-tiles"></div>
+            </div>
+        </div>
+    )
+}
