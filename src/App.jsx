ec852a40545f8f55d867371d564bc4242dfc8a02
Artyom Brazgovka <abrazgovka8@gmail.com>
2025-06-19 11:04:04 +0300

    refactor: some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-
 import {Hero} from "./components/Hero/Hero.jsx";
 import {Director} from "./components/Director/Director.jsx";
 import {Advantages} from "./components/Advantages/Advantages.jsx";
