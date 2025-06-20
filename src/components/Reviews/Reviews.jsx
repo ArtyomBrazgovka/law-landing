850cc5b7a410df826b1c7aa1c5a5618121392f13
Artyom Brazgovka <abrazgovka8@gmail.com>
2025-06-20 15:32:01 +0300

    feat: pagination and animation added

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,49 @@
+import {useState} from "react";
 import "./Reviews.css";
+import {Review} from "./Rewiew/Review";
 import julia from "/images/decorations/julia_vorobjova.webp";
+import director from "/images/decorations/director.webp";
+import anna_dmitrievna from "/images/decorations/anna_dmitrievna.webp";
 
 export function Reviews() {
+  const reviewsData = [
+    {
+      id: 1,
+      name: "Юлия Воробьева",
+      description: "Директор компании “Воробьева и ко”",
+      text: "Я обеспечу представление Ваших интересов в судах общей юрисдикции, арбитраже, третейском суде, приму участие в переговорах, окажу помощь на стадии досудебного урегулирования споров.",
+      photo: julia,
+    },
+    {
+      id: 2,
+      name: "Иван Иванов",
+      description: "CEO компании “Рога и копыта”",
+      text: "Отличный сервис! Очень доволен результатом. Рекомендую всем своим партнерам.",
+      photo: director,
+    },
+    {
+      id: 3,
+      name: "Анна Петрова",
+      description: "Глава какой-то корпорации",
+      text: "Быстро и качественно выполнили работу. Спасибо за профессионализм!",
+      photo: anna_dmitrievna,
+    },
+  ];
+
+  const [currentReviewIndex, setCurrentReviewIndex] = useState(0);
+
+  const nextReview = () => {
+    setCurrentReviewIndex((prevIndex) =>
+      prevIndex === reviewsData.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevReview = () => {
+    setCurrentReviewIndex((prevIndex) =>
+      prevIndex === 0 ? reviewsData.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className="reviews-section">
       <div className="reviews-container">
@@ -12,33 +54,34 @@ export function Reviews() {
           </h2>
         </div>
         <div className="reviews-content">
-          <button className="reviews-left-arrow">←</button>
+          <button className="reviews-left-arrow" onClick={prevReview}>
+            ←
+          </button>
           <div className="reviews-block">
             <div className="reviews-rectangle"></div>
             <div className="reviews-comment">
               <div className="reviews-first-quote">“</div>
               <div className="reviews-second-quote">„</div>
               <div className="reviews-cutout"></div>
-              <img className="reviews-photo" src={julia}></img>
-              <div className="reviews-review">
-                <p className="reviews-text">
-                  Я обеспечу представление Ваших интересов в судах общей
-                  юрисдикции, арбитраже, третейском суде, приму участие в
-                  переговорах, окажу помощь <br />
-                  на стадии досудебного урегулирования споров.
-                </p>
-                <div className="reviews-info">
-                  <h3 className="reviews-name">Юлия Воробьева</h3>
-                  <p className="reviews-desciption">
-                    Директор компании “Воробьева и ко”
-                  </p>
-                </div>
-              </div>
+              <Review {...reviewsData[currentReviewIndex]} />
             </div>
           </div>
-          <button className="reviews-right-arrow">→</button>
+          <button className="reviews-right-arrow" onClick={nextReview}>
+            →
+          </button>
         </div>
-        <div className="reviews-buttons"></div>
+        <div className="reviews-buttons">
+          {reviewsData.map((_, index) => (
+            <button
+              key={index}
+              style={{transform: `translateX(-${index * 100}%)`}}
+              className={`reviews-dot ${
+                index === currentReviewIndex ? "active" : ""
+              }`}
+              onClick={() => setCurrentReviewIndex(index)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
