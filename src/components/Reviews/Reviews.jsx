480fcbe06c2899ca529eff9241cb1c91e43bf670
Artyom Brazgovka <abrazgovka8@gmail.com>
2025-06-24 09:47:14 +0300

    refactor: moved reviwsData array to another file and imported clsx package

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,35 +1,10 @@
+import clsx from "clsx";
 import {useState} from "react";
 import "./Reviews.css";
-import {Review} from "./Rewiew/Review";
-import julia from "/images/decorations/julia_vorobjova.webp";
-import director from "/images/decorations/director.webp";
-import anna_dmitrievna from "/images/decorations/anna_dmitrievna.webp";
+import {Review} from "./Review/Review";
+import {reviewsData} from "./reviews-data";
 
 export function Reviews() {
-  const reviewsData = [
-    {
-      id: 1,
-      name: "Юлия Воробьева",
-      description: "Директор компании “Воробьева и ко”",
-      text: "Я обеспечу представление Ваших интересов в судах общей юрисдикции, арбитраже, третейском суде, приму участие в переговорах, окажу помощь на стадии досудебного урегулирования споров.",
-      photo: julia,
-    },
-    {
-      id: 2,
-      name: "Иван Иванов",
-      description: "CEO компании “Рога и копыта”",
-      text: "Отличный сервис! Очень доволен результатом. Рекомендую всем своим партнерам.",
-      photo: director,
-    },
-    {
-      id: 3,
-      name: "Анна Петрова",
-      description: "Глава какой-то корпорации",
-      text: "Быстро и качественно выполнили работу. Спасибо за профессионализм!",
-      photo: anna_dmitrievna,
-    },
-  ];
-
   const [currentReviewIndex, setCurrentReviewIndex] = useState(0);
 
   const nextReview = () => {
@@ -75,9 +50,9 @@ export function Reviews() {
             <button
               key={index}
               style={{transform: `translateX(-${index * 100}%)`}}
-              className={`reviews-dot ${
-                index === currentReviewIndex ? "active" : ""
-              }`}
+              className={clsx("reviews-dot", {
+                active: index === currentReviewIndex,
+              })}
               onClick={() => setCurrentReviewIndex(index)}
             />
           ))}
---
+++ src/components/Reviews/reviews-data.js
@@ -0,0 +1,27 @@
+import julia from "/images/decorations/julia_vorobjova.webp";
+import director from "/images/decorations/director.webp";
+import anna_dmitrievna from "/images/decorations/anna_dmitrievna.webp";
+
+export const reviewsData = [
+  {
+    id: 1,
+    name: "Юлия Воробьева",
+    description: "Директор компании “Воробьева и ко”",
+    text: "Я обеспечу представление Ваших интересов в судах общей юрисдикции, арбитраже, третейском суде, приму участие в переговорах, окажу помощь на стадии досудебного урегулирования споров.",
+    photo: julia,
+  },
+  {
+    id: 2,
+    name: "Иван Иванов",
+    description: "CEO компании “Рога и копыта”",
+    text: "Отличный сервис! Очень доволен результатом. Рекомендую всем своим партнерам.",
+    photo: director,
+  },
+  {
+    id: 3,
+    name: "Анна Петрова",
+    description: "Глава какой-то корпорации",
+    text: "Быстро и качественно выполнили работу. Спасибо за профессионализм!",
+    photo: anna_dmitrievna,
+  },
+];
