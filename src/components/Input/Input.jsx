524d0974f933dc64d933e092bdff6a088691010d
Artyom Brazgovka <abrazgovka8@gmail.com>
2025-06-25 10:57:51 +0300

    refactor: code formatted

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,16 +1,8 @@
 import "./Input.css";
 
 export function Input(props) {
-  
-
   const {type, ref} = props;
   return (
-    <input
-      type={type}
-      className="input"
-      ref={ref}
-      required
-      {...props}
-    ></input>
+    <input type={type} className="input" ref={ref} required {...props}></input>
   );
 }
